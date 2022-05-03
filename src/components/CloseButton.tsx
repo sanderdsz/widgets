63f15ff73daf8ad3dd7a37944e7ab31ab1b6dfd6
sanderdsz <sanderdsz@gmail.com>
2022-05-03 19:35:25 -0300

    components init and styles done

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,15 @@
+import {Popover} from '@headlessui/react'
+import {X} from "phosphor-react";
+
+export function CloseButton() {
+  return (
+    <div>
+      <Popover.Button
+        className='top-5 right-5 absolute text-zinc-400 hover:text-zinc-100'
+        title='Close feedback form'
+      >
+        <X weight='bold' className='w-4 h-4'/>
+      </Popover.Button>
+    </div>
+  )
+}
